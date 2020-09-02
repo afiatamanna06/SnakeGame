bbbad9e0b6ae4f8f1bd65410fbb1006a7407ba2b
afiatamanna06 <afiatamanna06@gmail.com>
2020-09-02 20:52:33 +0600

    app.py updated

--- app.py
+++ app.py
@@ -9,7 +9,13 @@ wn.tracer(0)
 head = turtle.Turtle()
 head.speed(0)
 head.shape("square")
-head.color("black")
+head.color("#ffffff")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+while True:
+    wn.update()
 
 
 wn.mainloop()
